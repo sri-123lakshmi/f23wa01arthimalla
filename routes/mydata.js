0d0ae9ea74154bf6e5f3f77c9b6558e4143dc12a
sri-123lakshmi <143051198+sri-123lakshmi@users.noreply.github.com>
2023-10-25 21:08:27 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Arthimalla Srilakshmi' });
+  res.render('mydata', { title: 'Arthimalla Srilakshmi' });
 });
 
 module.exports = router;
