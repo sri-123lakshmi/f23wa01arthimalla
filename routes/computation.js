73e003121c626642ba4481a8c30788b158e579c1
sri-123lakshmi <143051198+sri-123lakshmi@users.noreply.github.com>
2023-10-25 21:44:17 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    fake_url = "https://fake.com/path" + req.url
-    const url = new URL(fake_url)
-    const search_params = url.searchParams
-    if (req.method === 'GET') {
-        value = search_params.get("x")
-        if(value === null)
-        {
-            value=Math.round(Math.random()*999);
-        }     
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('Math.imul() applied to ' + value + ' is ' + Math.imul(2,value));
-        res.write('Math.log() applied to ' + value + ' is ' + Math.log(value))
-        res.write('Math.log10() applied to ' + value + ' is ' + Math.log10(value))
-        res.end()
+router.get('/', function (req, res, next){
+    var rand1 = Math.random().toFixed(2)
+    var rand2 = Math.random().toFixed(2)
+    var x = req.query.x;
+    var y = req.query.y;
+
+    if(y==undefined && x==undefined){
+        x = rand1
+        y = rand2
     }
 
+    imul = Math.imul(x,y).toFixed(2)
+    log= Math.log(x,y).toFixed(2)
+    log10= Math.log10(x,y).toFixed(2)
+    
+    res.send(`Math.imul(${x},${y}): ${imul} Math.log(${x}) is: ${log} Math.log10(${y}) is: ${log10}`);
 });
+    
+module.exports = router;
